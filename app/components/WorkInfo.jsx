c99b6ed083e7cb2ec8f7b1b7611a5a8406e1c2b5
Goldfinch <facu_vila@hotmail.com>
2025-04-21 16:21:23 -0300

    se refactorizo la seccion work

---
+++ app/components/WorkInfo.jsx
@@ -0,0 +1,12 @@
+const WorkInfo = () => {
+  return (
+    <>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12">
+        Welcome to my web development portfolio! Explore a collection of
+        projects showcasing my expertise in front-end development.
+      </p>
+    </>
+  );
+};
+
+export default WorkInfo;
